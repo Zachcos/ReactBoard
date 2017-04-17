a4c7ea5ec70c251c802886ea0a0cbfe88a3b13ec
Zach Cossman <Zachcos@gmail.com>
2017-04-17 19:20:26 -0400

    Add state to MessageBoardApp component

--- app/components/MessageBoardApp.jsx
+++ app/components/MessageBoardApp.jsx
@@ -1,6 +1,30 @@
 import React from 'react';
+import uuid from 'uuid';
 
 export class MessageBoardApp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      messages: [
+        {
+          id: uuid(),
+          subject: 'This is test message #1',
+          body: 'This is the first message'
+        },
+        {
+          id: uuid(),
+          subject: 'This is test message #2',
+          body: 'This is the second message'
+        },
+        {
+          id: uuid(),
+          subject: 'This is test message #3',
+          body: 'This is the third message'
+        },
+      ]
+    }
+  }
   render() {
     return (
       <p>This is the message board component</p>
