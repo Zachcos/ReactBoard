ef19a9c0c6e5f90fb77de1da0f475e73b5399304
Zach Cossman <Zachcos@gmail.com>
2017-04-18 12:51:12 -0400

    Prepare Message component to enable edit functionality and wrote updateMessage action/reducer

--- app/components/Message.jsx
+++ app/components/Message.jsx
@@ -1,23 +1,60 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 
 export class Message extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      isEditing: false,
       message: this.props.message
     };
+
+    this.toggleEdit = this.toggleEdit.bind(this);
+    this.updateMessageState = this.updateMessageState.bind(this);
+  }
+
+  updateMessageState(event) {
+    const field = event.target.name;
+    const message = this.state.message;
+    message[field] = event.target.value;
+    return this.setState({messsage});
+  }
+
+  saveMessage(event) {
+    event.preventDefault();
+    this.props.actions.updateMessage(this.state.message)
+  }
+
+  toggleEdit() {
+    this.setState({isEditing: !this.state.isEditing})
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.message.id !== nextProps.message.id) {
+      this.setState({message: nextProps.message});
+    }
   }
 
   render() {
     const {message} = this.props;
+
+    if (this.state.isEditing) {
+      return (
+        <div>
+          <h2>Edit Message</h2>
+        </div>
+      )
+    }
+
     return (
       <div>
         <h3>{message.subject}</h3>
         <p>{message.body}</p>
         <hr />
         <p>This message's id is: {message.id}</p>
+        <button className="btn btn-primary" onClick={this.toggleEdit}>Edit</button>
       </div>
     )
   }
@@ -34,4 +71,10 @@ const mapStateToProps = (state, ownProps) => {
   }
 }
 
-export default connect(mapStateToProps)(Message);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    actions: bindActionCreators(actions, dispatch)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Message);
--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -2,6 +2,11 @@ import {hashHistory} from 'react-router';
 
 export const messagesReducer = (state = [], action) => {
   switch (action.type) {
+    case 'UPDATE_MESSAGE':
+      return [
+        ...state.filter(message => message.id !== action.message.id),
+        action.message
+      ];
     default:
       return state;
   }
