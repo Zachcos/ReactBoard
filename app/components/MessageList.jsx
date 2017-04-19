d114453251156f127067a21853e3cdf1a5c2a3cc
Zach Cossman <Zachcos@gmail.com>
2017-04-19 19:55:18 -0400

    Add message to MessageList to inform users when there are no messages to display

--- app/components/MessageList.jsx
+++ app/components/MessageList.jsx
@@ -3,6 +3,15 @@ import {Link} from 'react-router';
 
 const MessageList = ({ messages }) => {
   const renderList = () => {
+
+    if (messages.length === 0) {
+      return (
+        <li className="list-group-item">
+          No messages to display
+        </li>
+      )
+    }
+
     return messages.map(message => {
       return (
         <li key={message.id} className="list-group-item">
