4cdcd835293051f4eacc901021935a2bc33444c3
Zach Cossman <Zachcos@gmail.com>
2017-04-20 13:41:32 -0400

    Update profile page to display information about the current user

--- app/components/Profile.jsx
+++ app/components/Profile.jsx
@@ -5,8 +5,17 @@ import firebase from 'app/firebase';
 
 export class Profile extends React.Component {
   render() {
-    return <p>This is the Profile component</p>
+    const currentUser = firebase.auth().currentUser;
+
+    return (
+      <div className="col-sm-6 col-sm-offset-3">
+        <h2>{currentUser.displayName}'s Profile</h2>
+        <p>Display name: {currentUser.displayName}</p>
+        <p>Email: {currentUser.email}</p>
+        <p>UserId: {currentUser.uid}</p>
+      </div>
+    )
   };
 };
 
-export default Profile;
+export default connect((state) => state)(Profile);
