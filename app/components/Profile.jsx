69bba638cb1be54f33601a7b4a324e92ed88aad4
Zach Cossman <Zachcos@gmail.com>
2017-04-23 18:32:54 -0400

    Wire up message list in Profile component

--- app/components/Profile.jsx
+++ app/components/Profile.jsx
@@ -6,13 +6,37 @@ import firebase from 'app/firebase';
 export class Profile extends React.Component {
   render() {
     const currentUser = firebase.auth().currentUser;
+    const {messages} = this.props;
+
+    const renderMessageList = () => {
+      return messages.map(message => {
+        if (currentUser.uid === message.userId) {
+          return (
+            <div key={message.id} className="panel panel-default">
+              <div className="panel-heading">
+                <h3 className="panel-title">{message.subject}</h3>
+              </div>
+              <div className="panel-body">
+                <p>{message.body}</p>
+                <p>{message.id}</p>
+              </div>
+            </div>
+          )
+        }
+      })
+    }
 
     return (
-      <div className="col-sm-6 col-sm-offset-3">
-        <h2>{currentUser.displayName}'s Profile</h2>
-        <p>Display name: {currentUser.displayName}</p>
-        <p>Email: {currentUser.email}</p>
-        <p>UserId: {currentUser.uid}</p>
+      <div className="col-sm-12">
+        <div className="col-sm-7">
+          <h2>{currentUser.displayName}'s Profile</h2>
+          <p>Display name: {currentUser.displayName}</p>
+          <p>Email: {currentUser.email}</p>
+          <p>UserId: {currentUser.uid}</p>
+        </div>
+        <div className="col-sm-5">
+          {renderMessageList()}
+        </div>
       </div>
     )
   };
