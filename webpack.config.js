3ff5b9544575ac1847c2bd59e0862d12bb99d727
Zach Cossman <Zachcos@gmail.com>
2017-04-25 14:46:31 -0400

    Reconfigure Webpack for v2, Add .babelrc file for presets

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,14 @@ var path = require('path');
 
 module.exports = {
   entry: [
-    'script!jquery/dist/jquery.min.js',
+    'script-loader!jquery/dist/jquery.min.js',
     './app/app.jsx'
   ],
   externals: {
-    jquery: 'jQuery'
+    jquery: 'jQuery',
+    'react/addons': true,
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true
   },
   plugins: [
     new webpack.ProvidePlugin({
@@ -20,8 +23,8 @@ module.exports = {
     filename: './public/bundle.js'
   },
   resolve: {
-    root: __dirname,
-    modulesDirectories: [
+    modules: [
+      __dirname,
       'node_modules',
       './app/components',
       './app/api'
@@ -33,24 +36,16 @@ module.exports = {
       reducers: 'app/reducers/reducers.jsx',
       configureStore: 'app/store/configureStore.jsx'
     },
-    extensions: ['', '.js', '.jsx']
+    extensions: ['.js', '.jsx']
   },
   module: {
-    loaders: [
+    rules: [
       {
         loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-0']
-        },
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/
       }
     ]
   },
-  // sassLoader: {
-  //   includePaths: [
-  //     path.resolve(__dirname, './node_modules/foundation-sites/scss')
-  //   ]
-  // },
   devtool: 'eval-source-map'
 };
