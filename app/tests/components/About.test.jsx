39fa018e5cb184c296f8b8fab45c7359a5445b98
Zach Cossman <Zachcos@gmail.com>
2017-04-26 01:06:19 -0400

    Add tests for About component

---
+++ app/tests/components/About.test.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import expect from 'expect';
+import {shallow} from 'enzyme';
+
+import About from 'About';
+
+describe('Component: About', () => {
+  const wrapper = shallow(<About />);
+
+  it('should render', () => {
+    expect(wrapper).toExist();
+  })
+
+  it('should render a header', () => {
+    expect(wrapper.find('h2').length).toBe(1);
+  })
+
+  it('should render a paragraph', () => {
+    expect(wrapper.find('p').length).toBe(1);
+  });
+});
