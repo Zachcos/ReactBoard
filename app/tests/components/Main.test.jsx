9109816509ed41b10a735392acd81a0d799f6bfe
Zach Cossman <Zachcos@gmail.com>
2017-04-25 20:12:54 -0400

    Update Main component test

--- app/tests/components/Main.test.jsx
+++ app/tests/components/Main.test.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 import expect from 'expect';
 
 import Main from 'Main';
+import Navigation from 'Navigation'
+import Home from 'Home';
 
 describe('Component: Main', () => {
-  const wrapper = shallow(<Main />);
+  const wrapper = shallow(<Main><Home /></Main>);
 
   it('should render', () => {
     expect(wrapper).toExist();
+  })
+
+  it('should render <Navigation /> component', () => {
+    expect(wrapper.find(Navigation)).toExist();
+  })
+
+  it('should render <Home /> as child', () => {
+    expect(wrapper.find(Home)).toExist();
   })
 })
