41e20824c6b60e4738b7a8cac982c01f40aae7fd
Zach Cossman <Zachcos@gmail.com>
2017-04-25 20:34:28 -0400

    Refactor tests to report correctly

--- app/tests/components/Main.test.jsx
+++ app/tests/components/Main.test.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
 import {shallow, mount} from 'enzyme';
 import expect from 'expect';
+var configureStore = require('configureStore');
+var {Provider} = require('react-redux');
 
 import Main from 'Main';
 import Navigation from 'Navigation'
 import Home from 'Home';
+import About from 'About';
 
 describe('Component: Main', () => {
-  const wrapper = shallow(<Main><Home /></Main>);
+  var store = configureStore.configure();
+  const wrapper = mount(
+    <Provider store={store}>
+      <Main>
+        <Home />
+        <About />
+      </Main>
+    </Provider>
+  );
 
   it('should render', () => {
     expect(wrapper).toExist();
   })
 
   it('should render <Navigation /> component', () => {
-    expect(wrapper.find(Navigation)).toExist();
+    expect(wrapper.find(Navigation).length).toBe(1)
   })
 
   it('should render <Home /> as child', () => {
-    expect(wrapper.find(Home)).toExist();
+    expect(wrapper.find(Home).length).toBe(1);
+  })
+
+  it('should render <About /> as child', () => {
+    expect(wrapper.find(About).length).toBe(1);
   })
 })
