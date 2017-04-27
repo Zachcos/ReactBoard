4180fe9273b57a4d7ac95136155ece9eb9e72596
Zach Cossman <Zachcos@gmail.com>
2017-04-27 10:02:17 -0400

    Add more MessageBoardApp tests

--- app/tests/components/MessageBoardApp.test.jsx
+++ app/tests/components/MessageBoardApp.test.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import expect from 'expect';
+import {Link} from 'react-router';
 import {shallow, mount} from 'enzyme';
 import {Provider} from 'react-redux';
 import {configure} from 'configureStore';
@@ -19,7 +20,11 @@ describe('Component: MessageBoardApp', () => {
     expect(wrapper).toExist();
   });
 
+  it('should render new message button', () => {
+    expect(wrapper.find('Link').text()).toBe('+ message');
+  });
+
   it('should render <MessageList /> component', () => {
     expect(wrapper.find(MessageList).length).toBe(1);
-  })
+  });
 });
