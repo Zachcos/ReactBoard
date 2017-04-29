0d7e4087afbc3bac2e3881b315a30ee332d301dd
Zach Cossman <Zachcos@gmail.com>
2017-04-29 15:17:39 -0400

    Add initial tests for MessageList component

---
+++ app/tests/components/MessageList.test.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import expect from 'expect';
+import {shallow, mount} from 'enzyme';
+import {Link} from 'react-router';
+
+import MessageList from 'MessageList';
+
+describe('Component: MessageList', () => {
+  const messages = [{
+    id: 1,
+    subject: 'this is a test',
+    body: 'still a test'
+  },
+  {
+    id: 2,
+    subject: 'this is a test',
+    body: 'still a test'
+  }]
+  const wrapper = shallow(<MessageList messages={messages}/>);
+
+  it('should exist', () => {
+    expect(wrapper).toExist();
+  });
+
+  it('should render a <li> for each message passed in', () => {
+    expect(wrapper.children().length).toBe(messages.length)
+  });
+
+  it('should render a <Link> for each <li>', () => {
+    expect(wrapper.find('Link').length).toBe(messages.length);
+  });
+});
